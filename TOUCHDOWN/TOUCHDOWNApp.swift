be9ec199491da6243fd44105319225550634ff9c
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-01-01 05:08:35 +0400

    project init

---
+++ TOUCHDOWN/TOUCHDOWNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOUCHDOWNApp.swift
+//  TOUCHDOWN
+//
+//  Created by IKAKOOO on 01.01.23.
+//
+
+import SwiftUI
+
+@main
+struct TOUCHDOWNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
